8fb0b395c34b8e5e7cb82551366ea8edff91787b
BAEKJungHo <designjava@naver.com>
2022-01-30 16:24:49 +0900

    Create Project

---
+++ src/src/test/java/me/baek/apicaller/ApiCallerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.baek.apicaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCallerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
